6c0332495e7e5f40c899d27d6e927810903ad3f2
atadsp <atadsp@gmail.com>
2020-12-26 04:05:50 -0500

    deck now generates based on json input

--- server/services/deck.ts
+++ server/services/deck.ts
@@ -3,21 +3,40 @@ type cards = keyof typeof cards;
 
 class DeckService {
     public GenerateDeck(): any[]{
-        const deck:any[] = [];
         const cardsObject = (cards as any);
+        const keys = Object.keys(cardsObject);
+        let deck: string[] = [];
 
-        this.GetRandomCard(deck, cardsObject)
-
-        return deck;
-
+        return this.GetRandomCard(cardsObject, keys, deck);
     }
 
-    public GetRandomCard(deck: any[], cardsObject: any): any{
-        const keys = Object.keys(cardsObject);
-        const chosenKey = keys[Math.floor(Math.random() * keys.length)];
-        const card = cardsObject[chosenKey];
-        console.log(card);
-        return card
+    private GetRandomCard(cardsObject: any, keys: string[], deck: string[]):  string[]{
+        if(keys.length > 0){
+            const chosenKey = keys[Math.floor(Math.random() * keys.length)];
+            const card = cardsObject[chosenKey];
+            const count = this.GetOccurrence(deck, chosenKey);
+
+            if(count < card.count){
+                console.log(chosenKey, count);
+                deck.push(chosenKey);
+                this.GetRandomCard(cardsObject, keys, deck);
+            } else {
+                const index = keys.indexOf(chosenKey);
+                if (index > -1) {
+                    keys.splice(index, 1);
+                }
+                this.GetRandomCard(cardsObject, keys, deck);
+            }
+        } else {
+            return deck;
+        }
+        return deck;
+    }
+
+    private GetOccurrence(array: string[], value: string) {
+        let count = 0;
+        array.forEach((v) => (v === value && count++));
+        return count;
     }
 
 }
